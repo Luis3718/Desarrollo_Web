cf21fda800ba190ff72459bb088d9ce0598e023f
Luis3718 <Alvarado3718@hotmail.com>
2022-06-12 00:28:09 -0500

    modifique la app de polls

--- Proyecto/Proyectoapp/Proyectoapp/urls.py
+++ Proyecto/Proyectoapp/Proyectoapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/", include("polls.urls"))
+    path("polls/", include("polls.urls")) #aqui damos la instruccion de ver las urls de nuestra app polls
 ]
